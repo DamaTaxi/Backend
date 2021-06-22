90f80ee4287c5b2ed9fe13104a7d3d65179e2c43
dohyeon5626 <kodohyeon71@gmail.com>
2021-06-22 11:41:41 +0900

    [PATCH] code, name

--- src/main/java/com/example/damataxi/domain/auth/service/DsmOauthAccountProvider.java
+++ src/main/java/com/example/damataxi/domain/auth/service/DsmOauthAccountProvider.java
@@ -2,6 +2,6 @@ package com.example.damataxi.domain.auth.service;
 
 import com.example.damataxi.domain.auth.retrofit.dto.DsmOauthResponse;
 
-public interface AccountProvider {
+public interface DsmOauthAccountProvider {
     DsmOauthResponse searchAccount(String oAuthToken);
 }
