6128551f9112247bfea46f2059a3d02981242a36
dohyeon5626 <67463759+dohyeon5626@users.noreply.github.com>
2021-09-03 21:24:32 +0900

    [PATCH] deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,10 +14,11 @@ if [ -z $CURRENT_PID ]
 then
   echo "> 종료할것 없음."
 else
-  echo "> kill -9 $CURRENT_PID"
-  sudo kill -15 $CURRENT_PID
-  echo "> kill -9 $JAR_PID"
-  sudo kill -15 $JAR_PID
+  echo "종료"
+  kill -9 $CURRENT_PID
+  kill -15 $CURRENT_PID
+  kill -9 $JAR_PID
+  kill -15 $JAR_PID
   sleep 5
 fi
 
