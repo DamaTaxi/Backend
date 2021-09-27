fd257d6859397f3b75306df213f87176c49c6b1c
dohyeon5626 <kodohyeon71@gmail.com>
2021-09-27 13:04:32 +0900

    [PATCH] change cors settings

--- src/main/java/com/example/damataxi/global/security/SecurityConfig.java
+++ src/main/java/com/example/damataxi/global/security/SecurityConfig.java
@@ -54,7 +54,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("*");
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 
     @Bean
