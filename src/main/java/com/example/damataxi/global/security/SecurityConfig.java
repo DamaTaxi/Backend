78ac4be3ddd1e768a3ca906f441d6349abeaa9af
dohyeon5626 <kodohyeon71@gmail.com>
2021-09-28 23:30:47 +0900

    [ADD] add unauthorization settings

--- src/main/java/com/example/damataxi/global/security/SecurityConfig.java
+++ src/main/java/com/example/damataxi/global/security/SecurityConfig.java
@@ -4,11 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -47,6 +49,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
                 .antMatchers(HttpMethod.DELETE, ApiPath.SUGGESTION_DELETE_PATH.getApiPath()).hasAuthority("ADMIN")
 
                 .anyRequest().authenticated()
+                .and().exceptionHandling()
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 .and().apply(new CorsConfigurer())
                 .and().apply(new JwtConfigure(jwtTokenProvider));
     }
